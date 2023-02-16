ffeac5ecba39e6ccfab13fad3e8ac5efc3c8c3b3
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-02-16 22:33:42 +0300

    Add files via upload

--- client/src/context/ChatContext.js
+++ client/src/context/ChatContext.js
@@ -1,1 +1,19 @@
+import { createContext, useContext, useState } from "react";
 
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([
+    { messages: "Selam" },
+    { messages: "Naber?" },
+  ]);
+
+  const values = {
+    messages,
+    setMessages,
+  };
+
+  return <ChatContext.Provider value={values}>{children}</ChatContext.Provider>;
+};
+
+export const useChat = () => useContext(ChatContext);
