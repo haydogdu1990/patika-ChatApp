f8b4be8c1ecb5e69df4aa4ebff09c1c114aae283
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-02-19 22:01:55 +0300

    Update redis.js

--- backend/clients/redis.js
+++ backend/clients/redis.js
@@ -2,7 +2,7 @@ const redis = require("redis");
 
 const getClient = () => {
   return redis.createClient({
-    host: process.env.REDIS_HOST,
+    url: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASS,
     db: 1,
